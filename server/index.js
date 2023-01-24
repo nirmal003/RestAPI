43aebae1e6ca589f3928f17759fd5ffad71f141f
maheshwaran-p <mpmahesh1022@gmail.com>
2023-01-24 18:18:06 +0530

    db-connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hi...this is home page");
 });
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect(process.env.DB_CONNECTION, console.log("DB"));
 
 app.listen(5000, (req, res) => {
   console.log("sever running on 5000 port");
